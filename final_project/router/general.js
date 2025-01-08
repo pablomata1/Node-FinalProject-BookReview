01b08c7d68c6b87ae2e6c6d43ae696b40240a3e1
pablomata1 <pablomata977@gmail.com>
2025-01-08 16:35:05 -0500

    finished project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,49 +26,46 @@ public_users.post("/register", (req,res) => {
 
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Syntax for  JSON.stringify(value[, replacer[, space]]);
-  res.send(JSON.stringify(books, null, 4));
+//Promise for getting books
+const getAllBooksPromise = () => {
+  return new Promise((resolve, reject) => {
+    resolve(books); 
+  }); 
+}
 
-});
-
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Get isbn from params in req object
-  const ISBN = req.params.isbn;
-  if(ISBN > 0 && ISBN < 11){
-    res.send(books[ISBN]);
-  } else{
-    res.send('No book found')
-  }
-
- });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Get author from params in req object
-  const author = (req.params.author).toLowerCase();
-
-  //Find author from the list of books
-  //Iterrate thru object 'books
-  let authorBooks = []; 
-  for (let book in books){
-    if((books[book].author).toLowerCase() === author){
-      authorBooks.push(books[book]);
+const getBookISBNPromise = (ISBN) => {
+  return new Promise((resolve, reject) => {
+    //Checks if object property exists, if true resovles book property
+    //Else rejects it with error message
+    if(books[ISBN]){
+      resolve(books[ISBN]);
+    } else{
+      reject('books is not available')
     }
-  }
+  })
+}
 
-  res.send(authorBooks);
+const getBookAuthorPromise = (author) => {
+  return new Promise((resolve, reject) => {
+    let authorBooks = []; 
+    for (let book in books){
+      if((books[book].author).toLowerCase() === author){
+        authorBooks.push(books[book]);
+        }
+    }
+    // Check to see if there is any results
+    if(authorBooks.length > 0){
+      resolve(authorBooks);
+    } else {
+      reject(`No books under author: '${author}' `);
+    }
+    
+  });
+}
 
-});
-
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Get title from params in the req object
-  const title = (req.params.title).toLowerCase();
-
-  //Find title from the list of books
+const getBooksTitlePromise = (title) => {
+ return new Promise((resolve, reject) => {
+   //Find title from the list of books
   //Iterrate thru object 'books
   let titleBooks = []; 
   for (let book in books){
@@ -77,8 +74,74 @@ public_users.get('/title/:title',function (req, res) {
     }
   }
 
-  res.send(titleBooks);
+  if(titleBooks.length > 0){
+    resolve(titleBooks);
+  } else {
+    reject(`No books found under title: '${title}'`);
+  }
+ });
+
+}
+
+// Get the book list available in the shop
+public_users.get('/',function (req, res) {
+  //Syntax for  JSON.stringify(value[, replacer[, space]]);
+  // res.send(JSON.stringify(books, null, 4));
+
+  //Using Promise
+  getAllBooksPromise().then(resolve => {
+    res.send(resolve)
+  }).catch(err => {
+    return res.status(404).json({Error: err});
+  })
+
+});
+
+
+// Get book details based on ISBN - using Async Await Promise 
+public_users.get('/isbn/:isbn', async (req, res) => {
+  //Get isbn from params in req obj
+  const ISBN = req.params.isbn;
   
+  //Try catch for any error, way to catch error with async await
+  try {
+    //awaits the response from promise
+      const data = await getBookISBNPromise(ISBN);
+      res.send(data);
+  } catch (err) {
+    res.send('error: ' + err);
+  }
+
+})
+
+// Get Book Details Based On Author - using Async Await Promise
+public_users.get('/author/:author', async (req, res) => {
+  //Get author from params in req object
+  const author = (req.params.author).toLowerCase();
+
+  //try catch for error handling from await promise
+  try {
+    //store resolve section of promise from 'getBookAuthorPromise'
+    const authorBookList = await getBookAuthorPromise(author);
+    res.send(authorBookList)
+  } catch (err) {
+    res.send('error: ' + err);
+  }
+
+});
+
+//GET ALL BOOKS BASED ON TITLE - USING ASYNC AWAIT PROMISE
+public_users.get('/title/:title', async (req, res) => {
+  //Get title from params 
+  const title = (req.params.title).toLowerCase();
+
+  try {
+    const bookList = await getBooksTitlePromise(title);
+    res.send(bookList);
+  } catch (err) {
+    res.send('error: ' + err);
+  }
+
 });
 
 //  Get book review
@@ -95,5 +158,59 @@ public_users.get('/review/:isbn',function (req, res) {
  
 
 });
+
+//SAME METHOD AS ABOVE WITHOUT ASYNC AWAIT PROMISE
+
+// Get Book Details Based On ISBN
+// public_users.get('/isbn/:isbn',function (req, res) {
+//   //Get isbn from params in req object
+//   const ISBN = req.params.isbn;
+//   if(ISBN > 0 && ISBN < 11){
+//     res.send(books[ISBN]);
+//   } else{
+//     res.send('No book found')
+//   }
+
+//  });
+
+
+// Get Book Details Based On Author
+// public_users.get('/author/:author',function (req, res) {
+//   //Get author from params in req object
+//   const author = (req.params.author).toLowerCase();
+
+//   //Find author from the list of books
+//   //Iterrate thru object 'books
+//   let authorBooks = []; 
+//   for (let book in books){
+//     if((books[book].author).toLowerCase() === author){
+//       authorBooks.push(books[book]);
+//     }
+//   }
+
+//   res.send(authorBooks);
+
+// });
+
+
+// Get all books based on title
+// public_users.get('/title/:title',function (req, res) {
+//   //Get title from params in the req object
+//   const title = (req.params.title).toLowerCase();
+
+//   //Find title from the list of books
+//   //Iterrate thru object 'books
+//   let titleBooks = []; 
+//   for (let book in books){
+//     if((books[book].title).toLowerCase() === title){
+//       titleBooks.push(books[book]);
+//     }
+//   }
+
+//   res.send(titleBooks);
+  
+// });
+
+
 
 module.exports.general = public_users;
